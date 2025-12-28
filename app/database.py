f527e894abe9f607138cf1a355942ccd375ca5bf
Ds8200 <ds8200h@gmail.com>
2025-12-29 00:38:49 +0200

    fixed env

--- app/database.py
+++ app/database.py
@@ -12,7 +12,7 @@ DB_USER = os.getenv("MYSQL_USER")
 DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 DB_NAME = os.getenv("MYSQL_DATABASE")
 # שם ה-Service של MySQL ב-Kubernetes (DNS פנימי)
-DB_HOST = "mysql-service"
+DB_HOST = os.getenv("MYSQL_HOST")
 
 # בניית כתובת חיבור למסד הנתונים
 # פורמט: mysql+pymysql://user:password@host:port/database
